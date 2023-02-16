a488aea8ea3dbf2169fa263f1d1aa1dc7e1725ca
Андрей Согомонян <AASogomonyan@sberbank.ru>
2023-02-16 11:08:03 +0300

    array printed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val jedi = arrayOf("Zidane", "Messi", "Ronaldo")
+    println(jedi[0]);
+    jedi[0] = "Some person"
+    println(jedi[0]);
 }
