f52831defaaea32f75c2c156d58dae3322393734
Андрей Согомонян <AASogomonyan@sberbank.ru>
2023-03-03 16:15:10 +0300

    Worked with readln()

--- src/main/kotlin/stepik/in_out/InExample.kt
+++ src/main/kotlin/stepik/in_out/InExample.kt
@@ -1,13 +1,7 @@
 package stepik.in_out
 
-fun main(args : Array<String>) {
-    print("Введите ваше имя: ")
-    val userName = readln()
-    print("Введите ваш e-mail: ")
-    val userEmail = readln()
-    print("Введите ваш адрес: ")
-    val userAddress = readln()
-    println("Ваше имя: " + userName)
-    println("Ваш E-mail: " + userEmail)
-    println("Ваш адрес: " + userAddress)
+fun main() {
+    val qs = readln()
+    println("На вопрос \"$qs\" у меня нет ответа.")
+
 }
